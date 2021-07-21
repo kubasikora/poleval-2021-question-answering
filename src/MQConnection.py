9336215bff5cd91e98f9806905ff3d0c391bc25f
Jakub Sikora <Jakub.Sikora1@ibm.com>
2021-07-21 16:35:50 +0200

    resolved problem with slow mq start by adding dependencies in compose file and reconnecting mechanism to MQadapter

--- src/MQConnection.py
+++ src/MQConnection.py
@@ -1,5 +1,7 @@
 import pika
 import logging
+from time import sleep
+from typing import Callable
 
 class MQConnection:
     logger = logging.getLogger("MQConnection")
@@ -8,19 +10,27 @@ class MQConnection:
         self.connection = None
         self.url = None
 
-    def connect(self, url) -> None:
-        if self.url is not None:
-            self.stop()
-        self.connection = pika.BlockingConnection(parameters=pika.URLParameters(url))
-        self.url        = url
-        self.channel    = self.connection.channel()
+    def connect(self, url: str) -> None:
+        reconnectCounter = 0
+        while reconnectCounter < 10:
+            try:
+                if self.url is not None:
+                    self.stop()
+                self.connection = pika.BlockingConnection(parameters=pika.URLParameters(url))
+                self.url        = url
+                self.channel    = self.connection.channel()
+                break
+            except:
+                MQConnection.logger.info(f"Reconnecting to {url}")
+                sleep(10)
+                reconnectCounter += 1
         MQConnection.logger.info(f"Connected to {self.url}")
 
     def stop(self) -> None:
         self.connection.close()
         MQConnection.logger.info("Stopped connection")
 
-    def register(self, queue, callback) -> None:
+    def register(self, queue: str, callback: Callable) -> None:
         self.channel.queue_declare(queue=queue)
         self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
         MQConnection.logger.info(f"Registered consumer to queue {queue}")
